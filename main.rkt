e09906ded8acc3471709035c36412ffe4b098503
daher13 <>
2023-08-29 12:11:11 -0300

    type system commit verification changed

--- main.rkt
+++ main.rkt
@@ -8,15 +8,15 @@
 
 (require "compiler/comp-peggen-peg.rkt")
 (require "compiler/comp-peg-lpeg.rkt")
-;; (require "type-system.rkt")
+(require "type-system.rkt")
 
-(define generated (sample (gen:peg 3 3 3) 100))
+;; (define generated (sample (gen:peg 3 3 3) 100))
 
 ;; example
-;; (define generated (term (((K 2 (T ϵ (S K ∅))) (• (/ 2 ϵ) 2) ()))))
-;; (define generated (term (((K T (T 2 ∅)) (/ K 2) ()))))
-;; (define generated (term (((E K (K ϵ ∅)) 1 ()))))
-;; (define generated (term (((D (* P) (P (• J 0) (J (* 0) ∅))) (/ D J) ((J #t ()) (P #f (J)) (D #t (P J)))))))
+(define generated (term (((K (! 2) (T K (S K ∅)))
+                          (• (* (! 2)) 2)
+                          ()))))
+;; (define generated (term (((S (/ B 2) (B (• 2 (• S 3)) ∅)) S ()))))
 
 (for ([ex (in-list generated)])
   (define peg (term (peggen->peg ,ex)))
@@ -24,8 +24,8 @@
   (define ppgpeg (car ex)) ;; peggen peg
   (define pgstart (cadr ex))
   (define pgtype (caddr ex))
-  ;; (define t (judgment-holds (ts ,lpeg 0 ,(car lpeg) () () t) t))
-  (define t (term ()))
+  ;; (define t (term ()))
+  (define t (judgment-holds (ts ,lpeg 0 ,(car lpeg) () () t) t))
   (printf "PG = ~a\nPGPEG = ~a\nPGStart = ~a\nPGType = ~a\nPEG = ~a\nLPEG = ~a\nType = ~a\n\n"
           ex ppgpeg pgstart pgtype peg lpeg t)
   )
--- type-system.rkt
+++ type-system.rkt
@@ -19,35 +19,37 @@
   [(fetch-i ilist pc) ,(list-ref (term ilist) (term pc))])
 
 (define-metafunction TypeSystem
-  sum : integer natural -> natural
-  [(sum integer natural) ,(+ (term integer) (term natural))])
+  sum : integer integer -> integer
+  [(sum integer_1 integer_2) ,(+ (term integer_1) (term integer_2))])
 
 (define-judgment-form TypeSystem
   #:mode (ts I I I I I O)
-
-  [
-   (where pc_1 (sum pc 1))
-   (ts ilist pc_1 (fetch-i ilist pc_1) pastl pastc t)
-   --------------------------------------------------------------------------- "T-emp"
-   (ts ilist pc emp pastl pastc t)
+  #:contract (ts ilist pc i pastl pastc t)
+  [ ;; fix when char matches and goto next
+   ----------------------------------------------------------------------------- "T-char"
+   (ts _ pc (char ch) pastl pastc (pastl pastc #f))
    ]
 
-  [
-   (where pc_1 (sum pc 1))
-   (ts ilist pc_1 (fetch-i ilist pc_1) pastl pastc (pastl_1 pastc_1 _))
-   --------------------------------------------------------------------------- "T-char"
-   (ts ilist pc (char _) pastl pastc (pastl_1 pastc_1 #f))
-   ]
+  ;; [
+  ;;  (where pc_1 (sum pc 1))
+  ;;  (ts ilist pc_1 (fetch-i ilist pc_1) pastl pastc (pastl_1 pastc_1 _))
+  ;;  --------------------------------------------------------------------------- "T-char"
+  ;;  (ts ilist pc (char _) pastl pastc (pastl_1 pastc_1 #f))
+  ;;  ]
 
   [
-   (where t (pastl pastc #f))
    --------------------------------------------------------------------------- "T-return"
-   (ts _ pc return pastl pastc t)
+   (ts _ pc return pastl pastc (pastl pastc #t))
    ]
 
   [
    --------------------------------------------------------------------------- "T-end"
-   (ts ilist pc end pastl pastc (pastl pastc #f))
+   (ts _ pc end pastl pastc (pastl pastc #t))
+   ]
+
+  [
+   --------------------------------------------------------------------------- "T-fail"
+   (ts _ pc fail pastl pastc (pastl pastc #t))
    ]
 
   [
@@ -58,7 +60,6 @@
    ]
 
   [
-   (side-condition ,(>= (term l) 0))
    ;; first option - goto label
    (where pc_1 (sum pc l))
    (where i_1 (fetch-i ilist pc_1))
@@ -68,46 +69,54 @@
    (where i_2 (fetch-i ilist pc_2))
    (ts ilist pc_2 i_2 pastl pastc ((l_2 ...) (c_2 ...) b_2))
    ;; results
-   (where pastl_f (l_1 ... l_2 ...))
-   (where pastc_f (c_1 ... c_2 ...))
-   (where b_f ,(and (term b_1) (term b_2)))
-   --------------------------------------------------------------------------- "T-call"
-   (ts ilist pc (choice l) pastl pastc (pastl_f pastc_f b_f))
-   ]
-
-  [
-   (side-condition ,(>= (term l) 0))
-   (where pc_1 (sum pc l))
-   (where i_1 (fetch-i ilist pc_1))
-   (ts ilist pc_1 i_1 pastl pastc t)
-   --------------------------------------------------------------------------- "T-commit"
-   (ts ilist pc (commit l) pastl pastc t)
-   ]
-
-  [
-   ;; first option - goto label
-   (where pc_1 (sum pc l))
-   (where i_1 (fetch-i ilist pc_1))
-   (ts ilist pc_1 i_1 pastl pastc ((l_1 ...) (c_1 ...) b_1))
-   ;; second option - goto next
-   (where pc_2 (sum pc 1))
-   (where i_2 (fetch-i ilist pc_2))
-   (ts ilist pc_2 i_2 pastl pastc ((l_2 ...) (c_2 ...) b_2))
-   ;; results
-   (where pastl_f (l_1 ... l_2 ...))
-   (where pastc_f (c_1 ... c_2 ...))
-   (where b_f ,(or (term b_1) (term b_2)))
+   (where pastl_3 (l_1 ... l_2 ...))
+   (where pastc_3 (c_1 ... c_2 ...))
+   (where b_3 ,(or (term b_1) (term b_2)))
    --------------------------------------------------------------------------- "T-choice"
-   (ts ilist pc (call l) pastl pastc (pastl_f pastc_f b_f))
+   (ts ilist pc (choice l) pastl pastc (pastl_3 pastc_3 b_3))
+   ]
+
+  [
+   (where pc_1 (sum pc l))
+   (side-condition ,(not (member (term pc_1) (term pastl))))
+   ;; first option - goto label
+   (where i_1 (fetch-i ilist pc_1))
+   (where pastl_1 ,(cond [(< (term l) 0) (append (term pastl)
+                                                 (term (pc_1))
+                                                 )]
+                         [else (term pastl)]))
+   (ts ilist pc_1 i_1 pastl_1 () ((l_1 ...) (c_1 ...) b_1))
+   ;; second option - goto next
+   ;; if next is onb pastc, ignore
+   (where pc_2 (sum pc 1))
+   (where i_2 (fetch-i ilist pc_2))
+   (ts ilist pc_2 i_2 pastl pastc ((l_2 ...) (c_2 ...) b_2))
+   ;; results
+   (where pastl_3 (l_1 ... l_2 ...))
+   (where pastc_3 (c_1 ... c_2 ...))
+   (where b_3 ,(and (term b_1) (term b_2)))
+   --------------------------------------------------------------------------- "T-call"
+   (ts ilist pc (call l) pastl pastc (pastl_3 pastc_3 b_3))
+   ]
+
+  [
+   (where pc_1 (sum pc l))
+   (side-condition ,(not (member (term pc_1) (term pastc))))
+   (where i_1 (fetch-i ilist pc_1))
+   (where pastc_1 ,(cond [(< (term l) 0) (append (term pastc)
+                                                 (term (pc_1))
+                                                 )]
+                         [else (term pastc)]))
+   (ts ilist pc_1 i_1 pastl pastc_1 (pastl_2 pastc_2 boolean_2))
+   ----------------------------------------------------------------------------------------- "T-commit"
+   (ts ilist pc (commit l) pastl pastc (pastl_2 pastc_2 boolean_2))
+   ]
+
+  [
+   (where pc_2 (sum pc l))
+   ------------------------------------------------------------------------------------------------- "T-commit-loop"
+   (ts ilist pc (commit l) pastl (pc_1 ... pc_2 pc_3 ...) (pastl (pc_1 ... pc_2 pc_3 ...) #t))
    ]
   )
 
 (provide (all-defined-out))
-
-;; (define ilist (term ((char 1) emp end)))
-;; (define i (car ilist))
-;; (judgment-holds (ts ,ilist 0 ,i () () t) t)
-
-(require "comp-peg-lpeg.rkt")
-
-(define lpeg (term (peg->lpeg ((S (/ B 1)) (B (• (• 2 S) 3))))))
