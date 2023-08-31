650254e893de9138b0bbaab6059b6eb5a49ed7fe
daher13 <>
2023-08-30 21:05:02 -0300

    type checking functions added

--- main.rkt
+++ main.rkt
@@ -2,7 +2,7 @@
 
 (require redex)
 (require rackcheck)
-(require peg-gen)
+(require peg-gen peg-gen/peg-gen-types)
 
 (require "compiler/comp-peggen-peg.rkt")
 (require "compiler/comp-peg-lpeg.rkt")
@@ -20,36 +20,52 @@
                          ;; ()
                          ;; ))))
 
-;; (define generated (sample (gen:peg 3 3 3) 100))
-
-(define generated (term (((E (• (/ (• ϵ 1) (* G)) (! (• ϵ 0))) (L (/ (• (• E 2) (/ 2 1)) (• (• E 2) (• ϵ E))) (G (• (• (/ 2 2) (• 1 E)) (/ (/ 2 L) (! 0))) ∅))) (• (• (/ L 0) (/ 2 1)) (/ (/ L 2) (• ϵ 2))) ((G . #(struct:TyPEG #f ())) (L . #(struct:TyPEG #f (G E))) (E . #(struct:TyPEG #t (G))))))))
+;; (define generated (list (list
+                    ;; '(E (• (/ (• ϵ 1) (* G)) (! (• ϵ 0))) (L (/ (• (• E 2) (/ 2 1)) (• (• E 2) (• ϵ E))) (G (• (• (/ 2 2) (• 1 E)) (/ (/ 2 L) (! 0))) ∅))) '(• (• (/ L 0) (/ 2 1)) (/ (/ L 2) (• ϵ 2))) (list (cons 'G (TyPEG #f '())) (cons 'L (TyPEG #f '(G E))) (cons 'E (TyPEG #t '(G)))))))
 
 ;; example
 ;; (set! generated (term (((K ϵ ∅)
 ;;                           ()))))
 ;; (define generated (term (((S (/ B 2) (B (• 2 (• S 3)) ∅)) S ()))))
 
-(define pgpeg (term ())) ;; peggen peg
-(define pgstart (term ())) ;; peggen start
-(define pgtype (term ())) ;; peggen type
-(define peg (term ())) ;; extracted peg
-(define lpeg (term ())) ;; compiled lpeg
-(define blist (term ()))
-(define lpegt (term ())) ;; lpeg type
 
-(for ([ex (in-list generated)])
-  (set! pgpeg (car ex)) ;; peggen peg
-  (set! pgstart (cadr ex))
-  (set! pgtype (caddr ex))
-  (set! peg (term (peggen->peg ,ex)))
-  (define compiled (term (peg->lpeg ,peg)))
-  (set! lpeg (car compiled))
-  (set! blist (cadr compiled))
-  (print-list lpeg)
-  ;; (define t (term ()))
-  (printf "PG = ~a\nPGPEG = ~a\nPGStart = ~a\nPGType = ~a\nPEG = ~a\nLPEG = ~a\n"
-          ex pgpeg pgstart pgtype peg lpeg)
-  (define pc 28)
-  (set! lpegt (judgment-holds (ts ,lpeg ,pc ,(list-ref lpeg pc) (() () #t) ot) ot))
-  (printf "Type = ~a\n\n" lpegt)
-  )
+(define (compare-types lpegt pgt)
+  (match (cons pgt lpegt)
+    [(cons _ '()) #f]
+    [(cons t (list (list _ _ b))) (eq? (nullable? t) b)]
+    [_ (error (string-append (~a pgt) (~a lpegt)))]
+    ))
+
+(define (check-b-type blist b lpeg pgtypes)
+           (let* (
+                  [pc (term (fetch-b-index ,blist ,(car b)))]
+                  [i (list-ref lpeg pc)]
+                  [lpegt (judgment-holds (ts ,lpeg ,pc ,i (() () #t) ot) ot)]
+                  [pgt (assoc (car b) pgtypes)])
+             (if pgt
+             (compare-types lpegt (cdr pgt))
+             #t)))
+
+(define (test-type peg)
+  (if (or (null? (caddr peg)) (eq? (car peg) '∅))
+      #t
+      (let* (
+             [pgtypes (caddr peg)]
+             [compiled-peg (term (peggen->peg ,peg))]
+             [compiled-lpeg (term (peg->lpeg ,compiled-peg))]
+             [lpeg (car compiled-lpeg)]
+             [blist (cadr compiled-lpeg)])
+                (andmap (lambda (b)
+                  (check-b-type blist b lpeg pgtypes))
+                blist))))
+
+(define-property types-match ([peg (gen:peg 3 3 2)]) (test-type peg))
+
+(check-property (make-config #:tests 10) types-match)
+
+(define error1 (list '(I (• ϵ 0) ∅) '(• I ϵ) (list (cons 'I (TyPEG #f '()) ))))
+(define error2 (list '(C (• (• 0 ϵ) (! 0)) (N (! (• 0 C)) (D (• (! C) (• 0 ϵ)) ∅)))
+                '(• (/ 0 C) (• N D))
+                (list (cons 'D (TyPEG #f '(C)))
+                      (cons 'N (TyPEG #t '()))
+                      (cons 'C (TyPEG #f '())))))
